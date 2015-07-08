e162f5a3606aa659de59b54e85673c00972b22bf
batroxapps <batroxapps@hotmail.com>
2015-07-08 11:39:19 +0200

    deleted bin and gen

--- bin/AndroidGameEngine-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/bartold/Documenten/development/java/android/engine/bin/AndroidGameEngine-debug-unaligned.apk : \
-/home/bartold/Documenten/development/java/android/engine/bin/AndroidGameEngine.ap_ \
-/home/bartold/Documenten/development/java/android/engine/bin/classes.dex \
--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/bartold/Documenten/development/java/android/engine/bin/AndroidManifest.xml : \
-/home/bartold/Documenten/development/java/android/engine/AndroidManifest.xml \
